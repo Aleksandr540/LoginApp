cbb85db3b0b497a6acee33e580e0008cd31a0e79
Aleksandr Kryukov <kryukov86@mail.ru>
2021-08-26 00:53:54 +0700

    Add func addAlerts without logic

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,9 +9,30 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func logInPressed() {
+       addAlert(with: "Invalid login or password", and: "Please enter correct login or password")
+        }
+    @IBAction func forgotNamePressed() {
+       addAlert(with: "Hello!", and: "Your name is UI")
+    }
+    @IBAction func forgotPasswordPressed() {
+        addAlert(with: "Hi!", and: "Your password is Kit")
+    }
+    
+    func addAlert (with title:String, and message:String){
+    let alertName = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alertName.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+    self.present(alertName, animated: true, completion: nil)
+    }
 }
+
+
+
 
