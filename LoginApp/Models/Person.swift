3b22846c6ec23fdcd9f988c245a54ca73fb1e456
Aleksandr Kryukov <kryukov86@mail.ru>
2021-08-30 05:26:49 +0700

    Add Tab and Navigation bars

---
+++ LoginApp/Models/Person.swift
@@ -0,0 +1,11 @@
+//
+//  Person.swift
+//  LoginApp
+//
+//  Created by Александр Крюков on 30.08.2021.
+//  Copyright © 2021 Александр Крюков. All rights reserved.
+//
+
+//struct Person {
+//    fields
+//}
