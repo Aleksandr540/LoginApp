e0ea4a716219414b4aee6c9bc3f7ba721d323ac3
Aleksandr Kryukov <kryukov86@mail.ru>
2021-08-30 05:51:51 +0700

    Add Models user and Person and hardcode data

--- LoginApp/Models/Person.swift
+++ LoginApp/Models/Person.swift
@@ -6,6 +6,17 @@
 //  Copyright © 2021 Александр Крюков. All rights reserved.
 //
 
-//struct Person {
-//    fields
-//}
+struct User {
+   let login = "UI"
+    let password = "Kit"
+    let person: Person
+    
+}
+
+struct Person {
+    let name = "Aleksandr"
+    let surname = "Kryukov"
+    let photosArray = ["IMG_4653.HEIC", "IMG_4034", "IMG_3234.HEIC", "IMG_1194.JPG", "IMG_1155.JPG", "adb7f57f-756f-4e2d-a310-b2e8e062389a", "IMG_4042.HEIC"]
+    let aboutMe = "Я из Новосибирска, обожаю горы Алтая, Киргизии и Казахстана и хочу ещё раз в Питер в этом году"
+    
+}
